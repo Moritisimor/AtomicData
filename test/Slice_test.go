4f46fdec2959152f16c2ecc64b6e4d23aa87f8ca
Moritisimor <devMoritisimor@proton.me>
2025-12-07 16:52:27 +0100

    Refined Slice_test even further

--- test/Slice_test.go
+++ test/Slice_test.go
@@ -2,16 +2,17 @@ package tests
 
 import (
 	"AtomicData/pkg/atomicslice"
-	"fmt"
+	"slices"
 	"sync"
 	"testing"
 )
 
 // Test for determining if the Atomic Slice is really thread-safe
 func TestSlice(t *testing.T) {
+	N := 10000
 	collection := atomicslice.From([]float32{})
 	wg := sync.WaitGroup{}
-	for i := range(50) {
+	for i := range(N) {
 		wg.Add(1)
 		go func(i float32) {
 			defer wg.Done()
@@ -20,8 +21,13 @@ func TestSlice(t *testing.T) {
 	}
 
 	wg.Wait()
-	for i := range(50) {
-		n, _ := collection.Get(i)
-		fmt.Println(n)
+	clonedSlice := collection.Clone()
+	if collection.Len() != N {
+		t.Error("The expected length is not met!")
+	}
+	for i := range(N) {
+		if !slices.Contains(clonedSlice, float32(i) * float32(i)) {
+			t.Error("Could not find expected element!")
+		}
 	}
 }
